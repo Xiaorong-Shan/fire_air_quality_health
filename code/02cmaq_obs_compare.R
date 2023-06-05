e34bdadd155c0430553203b87fc02a5f021ba856
lhenneman <lhenneman@gmail.com>
2023-06-05 14:20:56 -0400

    create and plot evaluations

--- code/02cmaq_obs_compare.R
+++ code/02cmaq_obs_compare.R
@@ -9,12 +9,11 @@ library( viridis)
 ## =========================================================== ##
 ## Function - calculate comparison the metrics
 ## =========================================================== ##
-eval.fn <- function( Yhat, Yact, mod.name){
+eval.fn <- function( Yhat, Yact){
   num.diff <- sum( Yhat - Yact, na.rm = T)
   abs.diff <- sum( abs( Yhat - Yact), na.rm = T)
   denom <- sum( Yact, na.rm = T)
-  metrics <- data.table( year = mod.name,
-                         N = length( Yhat),
+  metrics <- data.table( N = length( Yhat),
                          NMB = num.diff / denom,
                          NME = abs.diff / denom,
                          MB   = num.diff / length( Yhat),
@@ -27,157 +26,53 @@ eval.fn <- function( Yhat, Yact, mod.name){
 ## ====================================================================== ##
 ### read in monitor data 
 ## ====================================================================== ##
-cmaq_monitors_ny <- fread( 'data/cmaq_processed/cmaq_monitors_ny.csv')
+# load monitor cmaq data
+cmaq_monitors_ny <- fread( 'data/cmaq_processed/cmaq_monitors_ny.csv', drop = 'geometry')
+
+# aqs ozone data is in ppb, convert to ppm
+cmaq_monitors_ny[ `Units of Measure` == 'Parts per million', 
+                  `Arithmetic Mean` := `Arithmetic Mean` * 1000]
+
+# load the spatial data
+load( file = 'data/epa_aqs/aqs_sites_ny.RData')
+
+# merge monitor data with spatial data
+cmaq_monitors_ny[, ID := as.character( ID)]
+cmaq_monitors_ny.sf <- 
+  merge (cmaq_monitors_ny,
+         AQS_locs[, c( 'mID', 'ID')],
+         by = c( 'mID', 'ID'),
+         allow.cartesian = TRUE)
+
+## ====================================================================== ##
+### do the evaluation
+## ====================================================================== ##
+cmaq_monitors_eval_fire <- 
+  cmaq_monitors_ny[, eval.fn( withfire, `Arithmetic Mean`),
+                   by = .( name, year, `Parameter Code`)][, fire := 'withfire']
+cmaq_monitors_eval_nofire <- 
+  cmaq_monitors_ny[, eval.fn( nofire, `Arithmetic Mean`),
+                   by = .( name, year, `Parameter Code`)][, fire := 'nofire']
+
+# rbind together, melt for plotting
+cmaq_monitors_eval <- rbind( cmaq_monitors_eval_fire, cmaq_monitors_eval_nofire)
+cmaq_monitors_eval.m <- 
+  melt( cmaq_monitors_eval,
+        id.vars = c( 'name', 'year', 'fire', 'Parameter Code'))
+## ====================================================================== ##
+### plot the evaluation
+## ====================================================================== ##
+
+ggplot( cmaq_monitors_eval.m[variable != 'N'],
+        aes( y = value, x = year, fill = fire)) + 
+  geom_col( position = position_dodge()) + 
+  scale_fill_brewer( palette = 'Set2') +
+  facet_grid( variable ~ name, scales = 'free_y') + 
+  theme_minimal() + 
+  theme( panel.grid.minor = element_blank())
 
 
 
-# how big are the daily differences in NY?
-hist( cmaq_county_ny[name == 'daily8hrmax', firefire - fire_0firefire])
-hist( cmaq_county_ny[name == 'dailyavgs', firefire - fire_0firefire])
-
-# melt and plot
-cmaq_state_ny.m <- 
-  melt( cmaq_state_ny, id.vars = c( 'year', 'variable', 'name', 'geoid', 'state_name'),
-        variable.name = 'fire')
-cmaq_county_ny.m <- 
-  melt( cmaq_county_ny, id.vars = c( 'year', 'variable', 'name', 'geoid', 'state_name'),
-        variable.name = 'fire')
-
-cmaq_state_ny.m[, date := paste( year, variable)]
-
-ggplot( cmaq_state_ny.m,
-        aes( x = date, y = value, color = fire, group = fire)) + 
-  geom_line() + 
-  facet_wrap( . ~ name, ncol = 1)
-
-# combine all into brick: 11 layers in total
-rasterCMAQ_brick <- 
-  brick( file_dims)
-
-plot( rasterCMAQ_brick)
-
-plot( calc(rasterCMAQ_brick, sd))
-summary( mean( rasterCMAQ_brick))
-
-# convert rasterCMAQ_brick to sf object
-rasterCMAQ_brick.sf <-
-  rasterCMAQ_brick %>%
-  rasterToPolygons( ) %>%
-  st_as_sf %>%
-  as.data.table
-
-# download some US data
-states <- us_states()
-
-rasterCMAQ_brick.sf$geometry <-
-  st_transform( rasterCMAQ_brick.sf$geometry,
-                crs = st_crs( states))
-
-rasterCMAQ_brick.sf.m <-
-  as.data.table( rasterCMAQ_brick.sf) %>%
-  melt( id.vars = 'geometry',
-        variable.name = 'names',
-        value.name = 'PM2.5')
-
-#plot together
-cmaq <- 
-  ggplot( ) +
-  # add the disperser grid
-  geom_sf( data = rasterCMAQ_brick.sf.m,
-           aes( fill = PM2.5, 
-                color = PM2.5,
-                geometry = geometry)) +
-  # add state boundaries
-  geom_sf( data = states,
-           aes( geometry = geometry),
-           color = 'black', size = 1,
-           fill = NA,
-           inherit.aes = FALSE) +
-  # change the fill & color scale
-  scale_fill_viridis(  limits = c( 25, 50), oob = scales::squish) +
-  scale_color_viridis( limits = c( 25, 50), oob = scales::squish) +
-  # be sure to show 0 in the color scales
-  expand_limits( fill = 0, color = 0) +
-  # create panels for each day
-  # facet_wrap( . ~ month, ncol = 3) +
-  coord_sf( xlim = c( -125, -67), ylim = c( 25, 50)) + 
-  # set boundaries over NY
-  # coord_sf( xlim = c( -83, -69), ylim = c( 40, 45)) +
-  # set thematic elements
-  facet_wrap( . ~ names) +
-  theme_bw()+
-  labs( fill = expression( PM['2.5']*', µg'~m^{'-3'}),
-        color= expression( PM['2.5']*', µg'~m^{'-3'}))+
-  guides( fill = 'none') +
-  # ggtitle("Mean & SD of 11 models")  + 
-  theme(plot.title = element_text(size = 20, face = "bold")) + 
-  theme(rect = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = 'bottom',
-        strip.text = element_text( size = 20)) 
-
-ggsave('CMAQ.png', cmaq, width = 14, height=5, scale = 1.2)
-
-# take mean and standard deviation, convert to sf
-Mean_LH.sf <- 
-  calc( raster1940_brick, mean) %>%
-  rasterToPolygons( ) %>%
-  st_as_sf %>%
-  as.data.table
-SD_LH.sf <- 
-  calc( raster1940_brick, sd) %>%
-  rasterToPolygons( ) %>%
-  st_as_sf %>%
-  as.data.table
-
-Mean_LH.sf[, metric := 'Mean across 11 models']
-SD_LH.sf[, metric := 'Std. Dev. across 11 models']
-
-raster_plot.dt <- 
-  rbind( Mean_LH.sf, SD_LH.sf)
 
 
-raster_plot.dt$geometry <-
-  st_transform( raster_plot.dt$geometry,
-                crs = st_crs( states))
 
-# create the plot.
-# Mean <-
-ggplot( ) +
-  # add the disperser grid
-  geom_sf( data = raster_plot.dt,
-           aes( fill = layer, 
-                color = layer,
-                geometry = geometry)) +
-  # add state boundaries
-  geom_sf( data = states,
-           aes( geometry = geometry),
-           color = 'black', size = 1,
-           fill = NA,
-           inherit.aes = FALSE) +
-  # change the fill & color scale
-  scale_fill_viridis(  limits = c( 0, 10), oob = scales::squish) +
-  scale_color_viridis( limits = c( 0, 10), oob = scales::squish) +
-  # be sure to show 0 in the color scales
-  expand_limits( fill = 0, color = 0) +
-  # create panels for each day
-  # facet_wrap( . ~ month, ncol = 3) +
-  coord_sf( xlim = c( -125, -67), ylim = c( 25, 50)) + 
-  # set boundaries over NY
-  # coord_sf( xlim = c( -83, -69), ylim = c( 40, 45)) +
-  # set thematic elements
-  facet_wrap( . ~ metric) +
-  theme_bw()+
-  labs( fill = expression( PM['2.5']*', µg'~m^{'-3'}),
-        color= expression( PM['2.5']*', µg'~m^{'-3'}))+
-  guides( fill = 'none') +
-  # ggtitle("Mean & SD of 11 models")  + 
-  theme(plot.title = element_text(size = 20, face = "bold")) + 
-  theme(rect = element_blank(),
-        axis.text.x = element_blank(),
-        axis.text.y = element_blank(),
-        axis.ticks = element_blank(),
-        legend.position = 'bottom',
-        strip.text = element_text( size = 20)) 
